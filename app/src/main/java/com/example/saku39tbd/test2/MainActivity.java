cc0f512b8897931956c70060f3670788f718c879
appqol <gsaku3981@gmail.com>
2018-02-22 11:36:17 +0900

    first commit

---
+++ app/src/main/java/com/example/saku39tbd/test2/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.saku39tbd.test2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.textview);
+        final EditText editText = (EditText) findViewById(R.id.textbox);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(editText.getText().toString());
+            }
+        });
+
+    }
+}
